01f536ec6ec4a79ab2bd381d3d0d64a4c37b56b5
yasminzin6 <yaaz6@outlook.com>
2024-12-29 17:34:50 +0200

    adding Yasmin Zineldin.txt file

---
+++ Donia Ibrahim/lab3-6.js
@@ -0,0 +1,19 @@
+function square(num) {  //call back
+    return num * num;
+}
+
+function processArray(arr, fun) {     //higher order
+    for (var i = 0; i < arr.length; i++) {
+        console.log(fun(arr[i]));
+    }
+}
+var numbers = [1, 2, 3, 4, 5, 6];
+console.log(processArray(numbers, square));
+
+function D(){
+    console.log("D");
+
+}
+
+
+
